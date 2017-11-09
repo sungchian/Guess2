35ccb69cadd3e856f9be7aa4f3d50a2233bb72f8
sungchian <bnbn870829@gmail.com>
2017-11-09 16:25:53 +0800

    2017/11/9

--- app/src/main/java/com/fju/guess/MainActivity.java
+++ app/src/main/java/com/fju/guess/MainActivity.java
@@ -1,12 +1,16 @@
 package com.fju.guess;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
-    int n =
+    Random r = new Random();
+    int n = r.nextInt(100)+1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
     }
-    public void guess(View view){
-        EditText number = (EditText) findViewById(R.id.editText);
-        int guess = Integer.parseInt(number.getText().toString());
-        if(guess<n){
-            new Aler
+    public void guess(View view) {
+        EditText ednumber = (EditText) findViewById(R.id.edNumber);
+        int guess = Integer.parseInt(ednumber.getText().toString());
+        if (guess == n) {
+            new AlertDialog.Builder(this)
+                    .setMessage("correct")
+                    .setPositiveButton("ok", null)
+                    .show();
+        }
+        if (guess > n) {
+            new AlertDialog.Builder(this)
+                    .setMessage("大一點")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+        if (guess < n) {
+            new AlertDialog.Builder(this)
+                    .setMessage("小一點")
+                    .setPositiveButton("OK", null)
+                    .show();
         }
     }
-
-
 }
